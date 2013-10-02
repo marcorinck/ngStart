3cc5c2400a515006512be00eb52aa0d5474f352e
mrinck <654321>
2013-10-02 15:21:27 +0200

    deleted comment

--- src/main/modules/about/about.js
+++ src/main/modules/about/about.js
@@ -3,14 +3,6 @@ define(['angular', 'about/AboutController' ], function (angular, AboutController
 
 	var about = angular.module("about", ["ngRoute"]);
 
-//	about.config(["$routeProvider", function($routeProvider) {
-//		$routeProvider.when('/about/', {
-//			templateUrl: "modules/" + 'about/about.html',
-//			controller: AboutController
-//		});
-//
-//	}]);
-
 	about.config(["$routeProvider", function($routeProvider) {
 		$routeProvider.when('/about/', {
 			templateUrl: "modules/" + 'about/about.html',
