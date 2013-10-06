905f837a2afbd0b729dc0072e5a5ae858fcdf81f
Marco Rinck <marco.rinck@gmail.com>
2013-10-06 12:03:14 +0200

    sample controller unit test

---
+++ src/test/unit/ContactControllerTest.js
@@ -0,0 +1,29 @@
+/**
+ * the controller needs to be loaded explicitly with requireJS as the normal application only registers the
+ * controllers inside the route definitions, which are not evaluated during testing, so they are not known to angularJS
+ */
+define(["contact/ContactController"], function(ContactController) {
+
+	"use strict";
+	describe("the contactservice", function () {
+		var contactController, scope;
+
+		beforeEach(function () {
+			//load contact module, see http://docs.angularjs.org/api/angular.mock.inject
+			module("contact");
+
+			inject(["$rootScope", "$controller", function ($rootScope, $controller) {
+				//instantiate the controller with a newly created scope
+				scope = $rootScope.$new();
+				contactController = $controller(ContactController, {$scope: scope});
+				//this would fail -> $controller("ContactController", {$scope: scope});
+			}]);
+		});
+
+
+		it("should be the authors name", function () {
+			expect(scope.author).toBe("Marco Rinck");
+		});
+	});
+
+});
