c6ca0c2e93d710a01b5b6a7200ed1b6c42e93143
Marco Rinck <romulus23@gmail.com>
2014-07-10 21:24:35 +0200

    angular-translate was missing in the generated main.js in prod build. Removes standardRequireModules config option for now so that only one require() call is in main.js with a hard-coded dependency list.

--- src/main/modules/config/config.js
+++ src/main/modules/config/config.js
@@ -2,17 +2,14 @@ define(["angular", "config/configuration"], function(angular, configuration) {
    "use strict";
 
     var standardAngularModules = ["ngRoute", "about", "contact", "navbar", "pascalprecht.translate", "config"],
-        standardRequireModules = ["angular", "translate", "translate-static-loader", "translate-handler-log", "app"],
         config = angular.module('config', []);
 
 
 
     if (configuration.useMock) {
         standardAngularModules.push("ngMockE2E");
-        standardRequireModules.push("angular-mocks");
     }
     configuration.standardAngularModules = standardAngularModules;
-    configuration.standardRequireModules = standardRequireModules;
 
     config.constant("config", configuration);
 
