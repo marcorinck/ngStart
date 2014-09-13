1b733b821c459b1f66393f49e80331f875080d0e
Kevin Baker <kbaker@kev.io>
2014-09-13 16:37:58 -0700

    changed default state

--- src/app/modules/app.js
+++ src/app/modules/app.js
@@ -3,6 +3,6 @@ var app = angular.module('app',['ui.router','main','home','about']);
 
 app.config(function($stateProvider, $urlRouterProvider) {
 
-  $urlRouterProvider.otherwise("/main");
+  $urlRouterProvider.otherwise("/main/home");
 
 });
