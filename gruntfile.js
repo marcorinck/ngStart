8c33c9253418268b7c84ed4844c809fe83397b34
Marco Rinck <romulus23@gmail.com>
2014-11-10 21:24:41 +0100

    use grunt-bump

--- gruntfile.js
+++ gruntfile.js
@@ -214,9 +214,22 @@ module.exports = function (grunt) {
 				}
 			}
 		},
-		release: {
+		push: {
 			options: {
-				npm: false
+				files: ['package.json', 'bower.json'],
+				add: true,
+				addFiles: ['.'], // '.' for all files except ingored files in .gitignore
+				commit: true,
+				commitMessage: 'Release v%VERSION%',
+				commitFiles: ["-a"], // '-a' for all files
+				createTag: true,
+				tagName: 'v%VERSION%',
+				tagMessage: 'Version %VERSION%',
+				push: true,
+				pushTo: 'origin',
+				npm: true,
+				npmTag: 'Release v%VERSION%',
+				gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d' // options to use with '$ git describe'
 			}
 		}
 	});
